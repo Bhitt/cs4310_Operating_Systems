0a243d4b9d655fcac7bd4ec1154a041f4b04c437
bhitt <BrandenHitt@gmail.com>
2019-10-12 21:59:52 -0700

    +round robin with time slice 5
    Signed-off-by: bhitt <BrandenHitt@gmail.com>

--- RoundRobinScheduler_TS5/src/roundrobinscheduler_ts5/RoundRobinScheduler_TS5.java
+++ RoundRobinScheduler_TS5/src/roundrobinscheduler_ts5/RoundRobinScheduler_TS5.java
@@ -1,8 +1,8 @@
 /*
  * Author: Branden Hitt
- * Purpose: implement a round robin sceduler with time slice 2
+ * Purpose: implement a round robin scheduler with time slice 5
  */
-package roundrobinscheduler_ts2;
+package roundrobinscheduler_ts5;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,41 +12,40 @@ import java.util.Scanner;
  *
  * @author bhitt
  */
-public class RoundRobinScheduler_TS2 {
+public class RoundRobinScheduler_TS5 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // Create a queue
-        RRQueue<Job> rr2 = new RRQueue<>();
+        RRQueue<Job> rr5 = new RRQueue<>();
         //Determine test size (1-30)
-        int jobSize = 5;
+        int jobSize = 10;
         //enter the jobs into the queue
-        readJobs(rr2, "job.txt", jobSize);
+        readJobs(rr5, "job.txt", jobSize);
         //create timers
         int currentTime = 0;
         double avgTurnAround=0;
         //Scheduler gives slices of time to each until they are all done
-        while(!(rr2.isEmpty())){
+        while(!(rr5.isEmpty())){
             //give a time slice of 2 to the current process
-            rr2.front().setBurst(rr2.front().getBurst()-2);
+            rr5.front().setBurst(rr5.front().getBurst()-5);
             //check if process is done
-            if(rr2.front().getBurst()<=0){
+            if(rr5.front().getBurst()<=0){
                 //update the current time
-                currentTime+= rr2.front().getBurst()+2;
+                currentTime+= rr5.front().getBurst()+5;
                 //capture the turn around time
                 avgTurnAround+=currentTime;
-                System.out.println("Turnaround:"+avgTurnAround);
                 //pop off the current process
-                rr2.pop();
+                rr5.pop();
             }else{
                 //update the current time
-                currentTime+=2;
+                currentTime+=5;
                 //pop off the current process
-                Job temp = rr2.pop();
+                Job temp = rr5.pop();
                 //push the current process back to the end of the queue
-                rr2.push(temp);
+                rr5.push(temp);
             }
         }
         //compute the average turn around time
@@ -54,11 +53,11 @@ public class RoundRobinScheduler_TS2 {
         //round to 2 decimal places
         avgTurnAround = (double) Math.round(avgTurnAround * 100) / 100;
         //output
-        System.out.println("FCFS average turnaround for "+jobSize+" jobs: "
+        System.out.println("RR average turnaround for "+jobSize+" jobs: "
             +avgTurnAround+" ms");
     }
-
-  private static void readJobs(RRQueue<Job> rr2, String fileName, int numJobs){
+    
+    private static void readJobs(RRQueue<Job> rr5, String fileName, int numJobs){
         int jobCount=0;
         //attempt to open the file
         try{
@@ -68,7 +67,7 @@ public class RoundRobinScheduler_TS2 {
             while(reader.hasNextLine() && jobCount<numJobs){
                 //first line is just the job name  "Job 3"
                 //second line is the burst time    "12"
-      rr2.push(new Job(reader.nextLine(),Integer.parseInt(reader.nextLine())));
+      rr5.push(new Job(reader.nextLine(),Integer.parseInt(reader.nextLine())));
                 jobCount++;
             }
             //close the file reader
@@ -79,5 +78,4 @@ public class RoundRobinScheduler_TS2 {
             e.printStackTrace();
         }
     }
-    
 }
